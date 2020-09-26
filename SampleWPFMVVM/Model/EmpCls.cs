8de0509e5e140b8b926448562ff1f778584c2ae6
Andrew <35283726+switch900@users.noreply.github.com>
2020-09-26 10:09:05 -0700

    Add project files.

---
+++ SampleWPFMVVM/Model/EmpCls.cs
@@ -0,0 +1,44 @@
+﻿namespace SampleWPFMVVM.Entity
+{
+    class EmpCls
+    {
+        private int _empNo;
+        public int EmpNo
+        {
+            get { return _empNo; }
+            set
+            {
+                _empNo = value;
+            }
+        }
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+
+            }
+        }
+        private string _designation;
+        public string Designation
+        {
+            get { return _designation; }
+            set
+            {
+                _designation = value;
+            }
+        }
+        private string _department;
+        public string Department
+        {
+            get { return _department; }
+            set
+            {
+                _department = value;
+            }
+        }
+    }
+}
+//
